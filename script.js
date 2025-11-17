c6ff57a8268b96a26014232f04ac9c9274db0899
elifyilmaz00 <elifyilmaz1906@gmail.com>
2025-11-18 00:29:15 +0300

    feat(js): initialize script.js and select core DOM elements

---
+++ script.js
@@ -0,0 +1,5 @@
+const apiKey= "1cfdaa1dab347ff5c2bf074fe3d6b321";
+const sehirInput= document.querySelector("#sehir-input");
+const getirBtn= document.querySelector("#getir-btn");
+const weatherContainer= document.querySelector("#weather-container");
+
